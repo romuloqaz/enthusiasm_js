618b2e2f79901a1c88597cbd9cf0bfcccee5578a
Romulo Pereira <romulo.pereiracz@gmail.com>
2020-06-09 16:12:26 -0300

    configurando templates de email usanto html

--- modulo2/src/app/controllers/AppointmentController.js
+++ modulo2/src/app/controllers/AppointmentController.js
@@ -5,7 +5,6 @@ import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
-
 import Mail from '../../lib/Mail';
 
 class AppointmentController {
@@ -139,6 +138,11 @@ class AppointmentController {
                     as: 'provider',
                     attributes: ['name', 'email'],
                 },
+                {
+                    model: User,
+                    as: 'user',
+                    attributes: ['name'],
+                },
             ],
         });
 
@@ -157,14 +161,25 @@ class AppointmentController {
             });
         }
 
-        appointment.canceled_at = new Date();
+        appointment.canceled_at = NOW;
 
         await appointment.save();
 
         await Mail.sendMail({
             to: `${appointment.provider.name} <${appointment.provider.email}>`,
             subject: 'Agendamento cancelado',
-            text: 'Você tem um novo cancelamento',
+            template: 'cancellation',
+            context: {
+                provider: appointment.provider.name,
+                user: appointment.user.name,
+                date: format(
+                    appointment.date,
+                    "'dia' dd 'de' MMMM', às' H:mm'h'",
+                    {
+                        locale: pt,
+                    }
+                ),
+            },
         });
 
         return res.json(appointment);
