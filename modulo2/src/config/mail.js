41ca904d471034b854b02a735db59939745a25a5
Romulo Pereira <romulo.pereiracz@gmail.com>
2020-06-09 15:37:07 -0300

    configurando nodemailer para envio de email para cancelamento de agendamento de servicos

---
+++ modulo2/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
+    auth: {
+        user: '9e3c9f335f612e',
+        pass: '2fadc356cb2c1c',
+    },
+    default: {
+        from: 'Equipe <noreply@gobarber.com>',
+    },
+};
