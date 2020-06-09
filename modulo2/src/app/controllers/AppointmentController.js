3d235bc50173e484b2d6355b37dec9dfee7c0abf
Romulo Pereira <romulo.pereiracz@gmail.com>
2020-06-09 14:57:38 -0300

    listagem de notificações do prestador de serviço

--- modulo2/src/app/controllers/AppointmentController.js
+++ modulo2/src/app/controllers/AppointmentController.js
@@ -9,12 +9,16 @@ import Notification from '../schemas/Notification';
 class AppointmentController {
     async index(req, res) {
         const { page = 1 } = req.query;
+
         const appointments = await Appointment.findAll({
-            where: { user_id: req.userId, canceled_at: null },
+            where: {
+                user_id: req.userId,
+                canceled_at: null,
+            },
             order: ['date'],
-            attributes: ['id', 'date'],
             limit: 20,
             offset: (page - 1) * 20,
+            attributes: ['id', 'date'],
             include: [
                 {
                     model: User,
@@ -30,7 +34,6 @@ class AppointmentController {
                 },
             ],
         });
-
         return res.json(appointments);
     }
 
@@ -49,7 +52,16 @@ class AppointmentController {
         const { provider_id, date } = req.body;
 
         /**
-         * Check if provider is a provider
+         * Provider can't create appointment for itself
+         */
+        if (provider_id === req.userId) {
+            return res.status(401).json({
+                error: 'You can not create appointments for yourself',
+            });
+        }
+
+        /**
+         * Check if provider_id is a provider
          */
         const checkIsProvider = await User.findOne({
             where: {
@@ -64,21 +76,21 @@ class AppointmentController {
             });
         }
 
+        const hourStart = startOfHour(parseISO(date));
+
         /**
          * Check for past dates
          */
-        const hourStart = startOfHour(parseISO(date));
-
         if (isBefore(hourStart, new Date())) {
-            return res.status(400).json({
-                error: 'Past dates are not permitted',
-            });
+            return res
+                .status(400)
+                .json({ error: 'Past date are not permitted' });
         }
 
         /**
-         * Check date availability
+         * Check date availabity
          */
-        const checkAvaibility = await Appointment.findOne({
+        const checkAvailabitity = await Appointment.findOne({
             where: {
                 provider_id,
                 canceled_at: null,
@@ -86,28 +98,29 @@ class AppointmentController {
             },
         });
 
-        if (checkAvaibility) {
-            return res.status(401).json({
-                error: 'Appointment date is not available',
-            });
+        if (checkAvailabitity) {
+            return res
+                .status(400)
+                .json({ error: 'Appointment date is not available' });
         }
 
         const appointment = await Appointment.create({
             user_id: req.userId,
             provider_id,
-            date: hourStart,
+            date,
         });
 
         /**
-         * notificar prestador de servico
+         * Notify appointment provider
          */
         const user = await User.findByPk(req.userId);
         const formattedDate = format(
             hourStart,
             "'dia' dd 'de' MMMM', às' H:mm'h'",
-            { locale: pt }
+            {
+                locale: pt,
+            }
         );
-
         await Notification.create({
             content: `Novo agendamento de ${user.name} para o ${formattedDate}`,
             user: provider_id,
