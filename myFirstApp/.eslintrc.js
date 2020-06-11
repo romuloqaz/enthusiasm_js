2208a21911ea6957a466de2396bb7417ba90d4f6
Romulo Pereira <romulo.pereiracz@gmail.com>
2020-06-11 15:34:32 -0300

    adicionando e configurando reactotron

--- myFirstApp/.eslintrc.js
+++ myFirstApp/.eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
+    __DEV__: 'readonly'
   },
   parser: 'babel-eslint', // para entender as ultimas versões do EcmaScript
   parserOptions: {
