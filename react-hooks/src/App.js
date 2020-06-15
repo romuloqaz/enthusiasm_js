a4fbf694443c597404483f2ce13447287c4ff4d4
Romulo Pereira <romulo.pereiracz@gmail.com>
2020-06-15 11:15:48 -0300

    hook useState

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [tech, setTech] = useState(['ReactJS', 'ReactNative']);
+  const [newTech, setNewTech] = useState('');
+
+  function handleAdd() {
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }
+
   return (
-    <div>
-      <p>Hello World</p>
-    </div>
+    <>
+      <ul>
+        {tech.map((t) => (
+          <li key={t}>{t}</li>
+        ))}
+      </ul>
+      <input value={newTech} onChange={(e) => setNewTech(e.target.value)} />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
   );
 }
 
