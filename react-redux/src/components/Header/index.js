d35cf44356f88224b71cf37399ff23aabd4a0b8f
Romulo Pereira <romulo.pereiracz@gmail.com>
2020-06-15 12:29:06 -0300

    hooks com redux: editando a aplicação para utilar os hooks com o redux

--- react-redux/src/components/Header/index.js
+++ react-redux/src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import { MdShoppingBasket } from 'react-icons/md';
 
@@ -9,7 +9,9 @@ import logo from '../../assets/images/logo.svg';
 
 // import { Container } from './styles';
 
-function Header({ cartSize }) {
+export default function Header() {
+  const cartSize = useSelector(state => state.cart.length);
+
   return (
     <Container>
       <Link to="/">
@@ -25,7 +27,3 @@ function Header({ cartSize }) {
     </Container>
   );
 }
-
-export default connect(state => ({
-  cartSize: state.cart.length,
-}))(Header);
