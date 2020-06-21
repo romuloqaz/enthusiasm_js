4b00555a927c7bf7106136ca920a44d9011244e3
Romulo Pereira <romulo.pereiracz@gmail.com>
2020-06-21 15:19:54 -0300

    adicionando rotas privadas

---
+++ web/src/routes/Route.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Proptypes from 'prop-types';
+import { Route, Redirect } from 'react-router-dom';
+
+export default function RouteWrapper({
+  component: Component,
+  isPrivate,
+  ...rest
+}) {
+  const signed = false;
+
+  if (!signed && isPrivate) {
+    return <Redirect to="/" />;
+  }
+  if (signed && !isPrivate) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <Route {...rest} component={Component} />;
+}
+
+RouteWrapper.propTypes = {
+  isPrivate: Proptypes.bool,
+  component: Proptypes.oneOfType([Proptypes.element, Proptypes.func])
+    .isRequired,
+};
+
+RouteWrapper.defaultProps = {
+  isPrivate: false,
+};
