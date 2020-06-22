4f5cf4072f1722f72766fb689364f3826fa60aff
Romulo Pereira <romulo.pereiracz@gmail.com>
2020-06-22 18:56:59 -0300

    persistindo autenticação do usuario

--- web/src/App.js
+++ web/src/App.js
@@ -1,19 +1,26 @@
-import './config/ReactotronConfig';
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+
+import './config/ReactotronConfig';
+
 import Routes from './routes';
 import history from './services/history';
+
+import { store, persistor } from './store';
+
 import GlobalStyle from './styles/global';
-import store from './store';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyle />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
