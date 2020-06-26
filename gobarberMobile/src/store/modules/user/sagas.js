c897645f107484161742f622675af7a2bf65bf73
Romulo Pereira <romulo.pereiracz@gmail.com>
2020-06-25 22:00:05 -0300

    configurando redux

---
+++ gobarberMobile/src/store/modules/user/sagas.js
@@ -0,0 +1,31 @@
+import { Alert } from 'react-native';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import api from '~/services/api';
+import { updateProfileSuccess, updateProfilefailure } from './actions';
+
+export function* updateProfile({ payload }) {
+  try {
+    const { name, email, avatar_id, ...rest } = payload.data;
+
+    const profile = {
+      name,
+      email,
+      avatar_id,
+      ...(rest.oldPassword ? rest : {}),
+    };
+
+    const response = yield call(api.put, 'users', profile);
+
+    Alert.alert('Sucesso!', 'Perfil atualizado com sucesso');
+
+    yield put(updateProfileSuccess(response.data));
+  } catch (error) {
+    Alert.alert(
+      'Falha na atualização',
+      'Houve um erro na atualização do perfil, verifique os seus dados'
+    );
+    yield put(updateProfilefailure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
