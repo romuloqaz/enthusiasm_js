4e08eee7ca6e06b229f7285684a19b7b70bf68c7
Romulo Pereira <romulo.pereiracz@gmail.com>
2020-07-14 12:47:03 -0300

    criptografia de senha

--- backend-ts/src/services/CreateUserService.ts
+++ backend-ts/src/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
 import User from '../models/User';
 
 interface Request {
@@ -18,7 +19,14 @@ class CreateUserService {
     if (checkUserExists) {
       throw new Error('Email address already used');
     }
-    const user = usersRepository.create({ name, email, password });
+
+    const hashedPassword = await hash(password, 8);
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
 
     await usersRepository.save(user);
 
