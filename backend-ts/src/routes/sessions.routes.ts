1a117bda86117dc43cd49301e9ee2c790f267c00
Romulo Pereira <romulo.pereiracz@gmail.com>
2020-07-14 13:50:55 -0300

    gerar token jwt

--- backend-ts/src/routes/sessions.routes.ts
+++ backend-ts/src/routes/sessions.routes.ts
@@ -10,14 +10,14 @@ sessionsRouter.post('/', async (request, response) => {
 
     const authenticateUser = new AuthenticateUserServicefrom();
 
-    const { user } = await authenticateUser.execute({
+    const { user, token } = await authenticateUser.execute({
       email,
       password,
     });
 
     delete user.password;
 
-    return response.json({ user });
+    return response.json({ user, token });
   } catch (error) {
     return response.status(400).json({ message: error.message });
   }
--- backend-ts/src/services/AuthenticateUserService.ts
+++ backend-ts/src/services/AuthenticateUserService.ts
@@ -1,5 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
 import User from '../models/User';
 
 interface Request {
@@ -9,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -27,8 +30,14 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination');
     }
 
+    const token = sign({}, '17d1cf8230d5cfdb027f40a072e9d249', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return {
       user,
+      token,
     };
   }
 }
