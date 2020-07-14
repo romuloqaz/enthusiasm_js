caa0c4b2808151393a01a0d31f41445d407fdf61
Romulo Pereira <romulo.pereiracz@gmail.com>
2020-07-13 21:08:29 -0300

    criando repositorio typeorm

--- backend-ts/src/database/migrations/1594676651618-CreateAppointments.ts
+++ backend-ts/src/database/migrations/1594676651618-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1594676651618
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
