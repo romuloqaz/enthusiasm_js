1c4dd4ace14b4eb335d240e6901186a598a291d6
Romulo Pereira <romulo.pereiracz@gmail.com>
2020-07-14 12:41:33 -0300

    criação de cadastro de novo usuario

--- backend-ts/src/routes/index.ts
+++ backend-ts/src/routes/index.ts
@@ -1,20 +1,12 @@
 import { Router } from 'express';
 
 import appointmentsRoutes from './appointments.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRoutes);
 
-routes.post('/users', (request, response) => {
-  const { name, email } = request.body;
-
-  const user = {
-    name,
-    email,
-  };
-
-  return response.json(user);
-});
+routes.use('/users', usersRouter);
 
 export default routes;
